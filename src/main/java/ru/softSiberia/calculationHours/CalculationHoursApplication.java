e007fbbc5354969b063526d3decbfa570ccb2eb3
LAYENS <alex_job94@mail.ru>
2026-03-09 20:00:08 +0700

    Version 0.1. Инициализация.
    Создание проекта и подключение зависимостей.

---
+++ src/main/java/ru/softSiberia/calculationHours/CalculationHoursApplication.java
@@ -0,0 +1,13 @@
+package ru.softSiberia.calculationHours;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculationHoursApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculationHoursApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/softSiberia/calculationHours/CalculationHoursApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.softSiberia.calculationHours;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculationHoursApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
